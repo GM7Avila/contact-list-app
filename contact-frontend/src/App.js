5f33fde9c8101eee3ca197ff6064e95cbfe4589c
GM7Avila <guilherme.medeiros.avila@gmail.com>
2024-07-24 12:04:22 -0300

    display contact list

--- contact-frontend/src/App.js
+++ contact-frontend/src/App.js
@@ -1,19 +1,24 @@
-import { useEffect, useState } from 'react';
 import './App.css';
+import { useEffect, useState } from 'react';
 import { getAllContacts } from './api/ContactService';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+// components
+import Header from './components/Header';
+import ContactList from './components/ContactList';
 
 function App() {
 
   const [data, setData] = useState({});
   const [currentPage, setCurrentPage] = useState(0);
 
-  const getContacts = async(page = 0, size = 10) => {
+  const getContacts = async (page = 0, size = 10) => {
     try {
       setCurrentPage(page);
       const { data } = await getAllContacts(page, size);
       setData(data);
       console.log(data);
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
   }
@@ -22,10 +27,22 @@ function App() {
     getContacts();
   }, []);
 
+  const toggleModal = (show) => {
+    console.log('clicked!');
+  }
+
   return (
-    <div className="App">
-     <h1>Hello World</h1>
-    </div>
+    <>
+      <Header toggleModal={toggleModal} nbOfContacts={data.totalElements} />
+      <main className='main'>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Navigate to={'/contacts'} />} />
+            <Route path='/contacts' element={<ContactList data={data} currentPage={currentPage} getAllContacts={getContacts} />} />
+          </Routes>
+        </div>
+      </main>
+    </>
   );
 }
 
