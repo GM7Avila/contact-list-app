0f2254f80e96c888d9a3099b5837b626be06a94f
GM7Avila <guilherme.medeiros.avila@gmail.com>
2024-07-24 11:42:28 -0300

    CORS configuration

--- contact-frontend/src/App.js
+++ contact-frontend/src/App.js
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getAllContacts } from './api/ContactService';
 
 function App() {
+
+  const [data, setData] = useState({});
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const getContacts = async(page = 0, size = 10) => {
+    try {
+      setCurrentPage(page);
+      const { data } = await getAllContacts(page, size);
+      setData(data);
+      console.log(data);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
   return (
     <div className="App">
      <h1>Hello World</h1>
